7c7d0eb59f734ff0c0eefb17a64791373cf19cf9
Ruhamma Ashenafi <ruhammaashenafi202@gmail.com>
2023-03-25 19:10:24 +0300

    Add clearing and displaying functionalities

--- src/Dom.js
+++ src/Dom.js
@@ -2,7 +2,6 @@ import { blankProject, Project } from "./initProjectLoad";
 function element(type) {
   return document.createElement(`${type}`);
 }
-
 export function initDom() {
   //heading
   const content = document.querySelector(".content");
@@ -15,3 +14,41 @@ export function initDom() {
   leftDiv.textContent = blankProject().pe.name;
   content.appendChild(leftDiv);
 }
+
+export function displayForm() {
+  document.getElementById("addTodo").style.display = "";
+}
+
+export function addItemToCheck() {
+  const item = document.getElementById("addCheck").value;
+  if (item != "") {
+    const ul = document.querySelector(".todoUl");
+    const li = element("li");
+    li.textContent = item;
+    const span = element("span");
+    span.className = "removeItem";
+    const removeSymbol = document.createTextNode("\u00D7");
+    li.appendChild(span);
+    span.appendChild(removeSymbol);
+    ul.appendChild(li);
+    document.getElementById("addCheck").value = "";
+
+    console.log(
+      document.querySelectorAll("li"),
+      document.querySelectorAll("li").length
+    );
+    document.querySelectorAll("li").forEach((item) => {
+      item.addEventListener("click", () => {
+        item.remove();
+      });
+    });
+  } else return;
+}
+
+export function clearForm() {
+  for (let i = 0; i < document.querySelectorAll("li").length; i++) {
+    document.getElementById("addCheck")[i].reset();
+  }
+  document.getElementById("addTodo").reset();
+}
+
