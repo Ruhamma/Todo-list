162e229fcc00ff0d8b3cc05ce2f88072680eab07
Ruhamma Ashenafi <ruhammaashenafi202@gmail.com>
2023-03-25 18:14:34 +0300

    Add event listeners to display form,add item to checklist and reset form

--- src/index.js
+++ src/index.js
@@ -4,3 +4,15 @@ import { todo } from "./todo";
 blankProject();
 
 initDom();
+
+const eventModule = (() => {
+  //display form
+  const newTodo = document.querySelector(".addTodoBtn");
+  newTodo.addEventListener("click", displayForm);
+  //add item to checklist
+  const addCheck = document.querySelector(".addToCheck");
+  addCheck.addEventListener("click", displayForm);
+  //reset form
+  const reset = document.querySelector(".reset");
+  reset.addEventListener("click", clearForm);
+})();
