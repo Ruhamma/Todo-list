6369b992666da3047cfbeba8d0c885e7e9a2702b
Ruhamma Ashenafi <ruhammaashenafi202@gmail.com>
2023-03-27 17:51:34 +0300

    Create atorage class remove storage function

--- src/storage.js
+++ src/storage.js
@@ -1,19 +1,77 @@
 import { displayTodo } from "./Dom";
-export function setToStorage(name, idName) {
-  return localStorage.setItem(
-    `${name}`,
-    document.getElementById(`${idName}`).value
-  );
-}
-export function fetchFromStorage(name) {
-  return localStorage.getItem(`${name}`);
-}
-export function saveTodo({ title, desc, date, priority, checklistData }) {
-  setToStorage("Title", "title");
-  setToStorage("Description", "desc");
-  setToStorage("Date", "date");
-  setToStorage("Priority", "priority");
-  localStorage.setItem("Checklist", checklistData);
-  displayTodo();
-  return { title, desc, date, priority, checklistData };
+import Todo from "./todo";
+import Project from "./project";
+import List from "./List";
+
+export default class Storage {
+  save(data) {
+    localStorage.setItem("List", JSON.stringify(data));
+  }
+  fetchList() {
+    const list = Object.assign(
+      new List(),
+      JSON.parse(localStorage.getItem("List"))
+    );
+
+    list.setList(
+      list.getList().map((project) => Object.assign(new Project(), project))
+    );
+
+    list.getList().forEach((project) => {
+      project.setTodoArray(
+        project.getTodos().map((todo) => Object.assign(new Todo(), todo))
+      );
+    });
+    return list;
+  }
+  addProject(project) {
+    const list = Storage.fetchList();
+    list.addProject(project);
+    save(list);
+  }
+  addTodo(todo, pName) {
+    const list = Storage.fetchList();
+    list.getProject(pName).addTodo(todo);
+    save(list);
+  }
+  removeProject(project) {
+    const list = Storage.fetchList();
+    list.removeProject(project);
+    save(list);
+  }
+  removeTodo(todo, pName) {
+    const list = Storage.fetchList();
+    list.getProject(pName).removeTodo(todo);
+    save(list);
+  }
+  renameTodo(projectName, todoName, newName) {
+    const list = Storage.fetchList();
+    list.getProject(projectName).getTodo(todoName).setTitle(newName);
+    Storage.saveTodoList(list);
+  }
+  renameProject(projectName, newName) {
+    const list = Storage.fetchList();
+    list.getProject(projectName).setName(newName);
+    Storage.saveTodoList(list);
+  }
+  changeDesc(projectName, todoName, newDesc) {
+    const list = Storage.fetchList();
+    list.getProject(projectName).getTodo(todoName).setDesc(newDesc);
+    Storage.saveTodoList(list);
+  }
+  ChangeDate(projectName, todoName, newDate) {
+    const list = Storage.fetchList();
+    list.getProject(projectName).getTodo(todoName).setDate(newDate);
+    Storage.saveTodoList(list);
+  }
+  changePriority(projectName, todoName, newPriority) {
+    const list = Storage.fetchList();
+    list.getProject(projectName).getTodo(todoName).setDesc(newPriority);
+    Storage.saveTodoList(list);
+  }
+  ChangeCheckList(projectName, todoName, newChecklist) {
+    const list = Storage.fetchList();
+    list.getProject(projectName).getTodo(todoName).setDate(newChecklist);
+    Storage.saveTodoList(list);
+  }
 }
